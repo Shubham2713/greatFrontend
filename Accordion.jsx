a71ba7a7d37e24763c760ae02ca3566f8841ee86
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2026-05-04 16:52:54 -0700

    Update Accordion.jsx

--- Accordion.jsx
+++ Accordion.jsx
@@ -1,69 +1,46 @@
 https://www.greatfrontend.com/questions/user-interface/accordion
 
+
 import {useState} from "react"
+
+export default function Accordion() {
+  const [toggleIndex, setToggleIndex] = useState({})
 
 contents= [{key: "HTML", value:"The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser."},
 {key: "CSS", value:"Cascading Style Sheets is a style sheet language used for describing the presentation of a docume written in a markup language such as HTML or XML."},
 {key: "JavaScript", value:"JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS."}]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-export default function Accordion( ){
-  const [openSections, setOpenSections] = useState({})
-
-function handleClick(index) {
-setOpenSections(currentSections=> {
-  return {...currentSections, [index]: !currentSections[index]}
-})
+function handClick(index) {
+  setToggleIndex(current=> {
+    return {...current, [index]: !current[index]}
+  })
 }
-  
-  
+
+console.log(toggleIndex)
+
   return (
+     
     <div>
-    {contents.map((content, index)=> {
-      isOpen= openSections[index];
-      return (<div key={index}>
-        <button onClick={()=> handleClick(index)} >
-          {content.key}{' '}
+      {contents.map((content, index)=>  { 
+        let isOpen= toggleIndex[index]
+        return <div key={index}>
+        <div >
+        <button onClick={()=> handClick(index)}>
+          {content.key}
+          </button>
           <span
             aria-hidden={true}
-            className={!isOpen?"accordion-icon": "accordion-icon accordion-icon--rotated"}
+            className= {!isOpen? "accordion-icon": "accordion-icon accordion-icon--rotated"}
           />
-        </button>
-        {isOpen && <div>
+        </div>
+        {isOpen &&<div>
           {content.value}
         </div>}
-      </div>)
-    })}
-    
-          </div>
+      </div>})}
+      
+    </div>
   );
 }
-
-
 
 
 

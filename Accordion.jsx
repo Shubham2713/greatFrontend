cafebfca232fd2e6c4e551b4c5b88a6a69bbeb22
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2026-05-04 16:33:49 -0700

    Update Accordion.jsx

--- Accordion.jsx
+++ Accordion.jsx
@@ -1,41 +1,50 @@
 https://www.greatfrontend.com/questions/user-interface/accordion
-import {useState} from "react"
+import { useState } from "react";
 
-contents= [{key: "HTML", value:"The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser."},
-{key: "CSS", value:"Cascading Style Sheets is a style sheet language used for describing the presentation of a docume written in a markup language such as HTML or XML."},
-{key: "JavaScript", value:"JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS."}]
+const contents = [
+  {
+    key: "HTML",
+    value:
+      "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.",
+  },
+  {
+    key: "CSS",
+    value:
+      "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML.",
+  },
+  {
+    key: "JavaScript",
+    value:
+      "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.",
+  },
+];
 
+export default function Accordion() {
+  const [openKey, setOpenKey] = useState(null);
 
+  function handleClick(key) {
+    if (openKey === key) {
+      setOpenKey(null);
+    } else {
+      setOpenKey(key);
+    }
+  }
 
-export default function Accordion( ){
-  const [openSections, setOpenSections] = useState({})
-
-function handleClick(index) {
-setOpenSections(currentSections=> {
-  return {...currentSections, [index]: !currentSections[index]}
-})
-}
-  
-  
   return (
     <div>
-    {contents.map((content, index)=> {
-      isOpen= openSections[index];
-      return (<div key={index}>
-        <button onClick={()=> handleClick(index)} >
-          {content.key}{' '}
-          <span
-            aria-hidden={true}
-            className={!isOpen?"accordion-icon": "accordion-icon accordion-icon--rotated"}
-          />
-        </button>
-        {isOpen && <div>
-          {content.value}
-        </div>}
-      </div>)
-    })}
-    
-          </div>
+      {contents.map((content) => (
+        <div key={content.key}>
+          <button onClick={() => handleClick(content.key)}>
+            {content.key} {openKey === content.key ? "▲" : "▼"}
+          </button>
+
+          {openKey === content.key && (
+            <div>
+              {content.value}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
   );
 }
-

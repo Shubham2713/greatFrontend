3c5102e6bc52c4b65795eef139730ff911aa9537
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2026-05-04 15:43:02 -0700

    Update To Do List.jsx

--- To Do List.jsx
+++ To Do List.jsx
@@ -1,43 +1,54 @@
 //https://www.greatfrontend.com/questions/user-interface/todo-list
-import { useState } from "react";
+import {useState } from "react"
 
 export default function App() {
-  const [value, setValue] = useState("");
-  const [tasks, setTasks] = useState([]);
+  const [val, setVal] = useState("")
+  const [tasks, setTasks] = useState([])
 
+  function handleClick() {
+    if(val==="")
+    return
+    let data=val.trim()
 
-  function handleSubmit() {
-    if (value.trim() === "") return
-    else
     setTasks(currentTasks=> {
+      return [...currentTasks, {name: data, id: crypto.randomUUID()}]
+    })
 
-      return [...currentTasks, {taskName: value.trim(), id: crypto.randomUUID()}]
-  })
-   setValue("")
+    setVal("")
+
   }
 
-function handleDelete(id) {
-  setTasks(currentTasks=>currentTasks.filter(task=>task.id!==id))
-}
+  function handleDelete(id)
+  {
+    setTasks(currentTasks=> { return currentTasks.filter(currentTask => {if(currentTask.id!==id) return currentTask
+    else
+    return})
+    })
+  }
+
+  console.log(tasks)
 
 
   return (
     <div>
       <h1>Todo List</h1>
       <div>
-        <input type="text" placeholder="Add your task" value={value} onChange={e=> setValue(e.target.value)}/>
+        <input type="text" placeholder="Add your task" value={val} onChange={e=> setVal(e.target.value)} />
         <div>
-          <button onClick={()=> handleSubmit()}>Submit</button>
+          <button onClick={handleClick}>Submit</button>
         </div>
       </div>
       <ul>
-        {tasks.map(task => {
+      {tasks.map(task=> {
         return <li key={task.id}>
-          <span>{task.taskName}</span>
-          <button onClick={()=>handleDelete(task.id)}>Delete</button>
+          <span>{task.name}</span>
+          <button onClick={()=> handleDelete(task.id)}>Delete</button>
         </li>
-        })}
+      })}
+        
       </ul>
     </div>
   );
 }
+
+

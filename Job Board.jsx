8855592dcb2d1d253245db415071fa01740d80a2
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2026-05-04 20:01:10 -0700

    Update Job Board.jsx

--- Job Board.jsx
+++ Job Board.jsx
@@ -1,4 +1,93 @@
 //https://www.greatfrontend.com/questions/user-interface/job-board
+import { useState, useEffect } from 'react';
+
+const val=[{
+  "by": "jamilbk",
+  "id": 35908337,
+  "score": 1,
+  "time": 1683838872,
+  "title": "Firezone (YC W22) is hiring Elixir and Rust engineers",
+  "type": "job",
+  "url": "https://www.ycombinator.com/companies/firezone/jobs"
+},
+{
+  "by": "jamilbk",
+  "id": 35908337,
+  "score": 1,
+  "time": 1683838872,
+  "title": "Firezone (YC W22) is hiring Elixir and Rust engineers",
+  "type": "job",
+  "url": "https://www.ycombinator.com/companies/firezone/jobs"
+}
+]
+
+const Base= 'https://hacker-news.firebaseio.com/v0/'
+const Increment= 6
+
+function timeFormat(data)
+{
+  
+  return new Date(data*1000).toLocaleString();
+
+}
+
+export default function App() {
+  const [userId, setUserId] = useState(null)
+  const [userData, setUserData] = useState([])
+  const [currentPage, setCurrentPage] = useState(0)
+  const [fetchingDetails, setFetchingDetails] = useState(false)
+
+useEffect(() => {
+    getData(0);
+  }, []);
+  async function getData(currentPage) {
+
+    let itemList= userId
+
+    if(itemList===null)
+    {
+    let data= await fetch(`${Base}/jobstories.json`)
+    let resp= await data.json();
+    setUserId(resp);
+    itemList=resp
+
+    }
+
+    let listToFetch= itemList.slice(Increment*currentPage, Increment*currentPage+Increment)
+
+    const itemsPage = await Promise.all(listToFetch.map(async list=> { let data= await fetch(`${Base}/item/${list}.json`)
+      let resp= await data.json()
+      return resp
+    })) 
+
+    setUserData(currentUserData=> [...currentUserData, ...itemsPage])
+
+    setCurrentPage(currentPage)
+    //console.log(resp)
+  }
+
+
+
+  return (
+    <>
+    <h1 className="title-head">Hacker News Jobs Board</h1>
+    <div>
+    {userData.map(value=> <div key={value.id}><h3>{value.title}</h3>
+  
+    <p>{`By ${value.by}  ${timeFormat(value.time)}`}</p>
+    </div>)}
+    <button onClick={()=> getData(currentPage+1)}>Load More</button>
+    </div>
+    </>
+  );
+}
+
+
+
+
+
+
+
 import { useState, useEffect } from "react";
 
 const BASE_URL = "https://hacker-news.firebaseio.com/v0";

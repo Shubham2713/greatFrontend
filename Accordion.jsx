10d7e83570e62d680a611a61bc835e4195d362fd
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2026-05-04 19:10:03 -0700

    Update Accordion.jsx

--- Accordion.jsx
+++ Accordion.jsx
@@ -44,54 +44,49 @@ console.log(toggleIndex)
 
 
 
-
-
-import { useState } from "react";
-
-const contents = [
-  {
-    key: "HTML",
-    value:
-      "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.",
-  },
-  {
-    key: "CSS",
-    value:
-      "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML.",
-  },
-  {
-    key: "JavaScript",
-    value:
-      "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.",
-  },
-];
+import {useState} from "react"
 
 export default function Accordion() {
-  const [openKey, setOpenKey] = useState(null);
+  const [toggleIndex, setToggleIndex] = useState(null)
 
-  function handleClick(key) {
-    if (openKey === key) {
-      setOpenKey(null);
-    } else {
-      setOpenKey(key);
-    }
+contents= [{key: "HTML", value:"The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser."},
+{key: "CSS", value:"Cascading Style Sheets is a style sheet language used for describing the presentation of a docume written in a markup language such as HTML or XML."},
+{key: "JavaScript", value:"JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS."}]
+
+function handClick(index) {
+  if(toggleIndex===index)
+  {
+    setToggleIndex(null)
   }
+  else
+  {
+    setToggleIndex(index)
+  }
+}
+
+console.log(toggleIndex)
 
   return (
+     
     <div>
-      {contents.map((content) => (
-        <div key={content.key}>
-          <button onClick={() => handleClick(content.key)}>
-            {content.key} {openKey === content.key ? "▲" : "▼"}
+      {contents.map((content, index)=>  { 
+        let isOpen= toggleIndex
+        return <div key={index}>
+        <div >
+        <button onClick={()=> handClick(index)}>
+          {content.key}
           </button>
-
-          {openKey === content.key && (
-            <div>
-              {content.value}
-            </div>
-          )}
+          <span
+            aria-hidden={true}
+            className= {isOpen!==index? "accordion-icon": "accordion-icon accordion-icon--rotated"}
+          />
         </div>
-      ))}
+        {isOpen===index &&<div>
+          {content.value}
+        </div>}
+      </div>})}
+      
     </div>
   );
 }
+
